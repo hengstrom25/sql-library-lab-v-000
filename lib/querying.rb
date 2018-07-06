07ad6612aadc30b733737c510cbc263a28d10aac
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-06 23:19:23 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors, subgenres
   JOIN series
-  
+  ON authors.id = series.author_id
 end
 
 def select_series_title_with_most_human_characters
