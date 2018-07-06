6e45cf17c24f2572114e9965a76ffde917684c48
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-06 23:28:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
   "SELECT title
   FROM series 
   JOIN characters
-  ON series.id = series.character_id
+  ON series_id = series.character_id
   WHERE species = 'human'
   LIMIT 1"
 end
