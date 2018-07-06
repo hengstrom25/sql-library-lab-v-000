0934a3362a8b8871210ba27e0974ef4ddf5a1e25
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-06 23:01:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,7 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT species
+  "SELECT species, COUNT (species)
   FROM characters
   GROUP BY species
   ORDER BY COUNT(*) DESC
