6926e22cae6d8418b12ea3f7b035c122d6e521ec
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-06 23:34:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,5 +38,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT (character.books, character_id)"
+  "SELECT characters.name, COUNT (character.books.character_id)
+  FROM books
+  "
 end
