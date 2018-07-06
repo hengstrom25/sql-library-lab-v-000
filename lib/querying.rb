3a96edaab5e123fac8adf08797b79210e0c250fa
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-06 23:26:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT
+  "SELECT series.title, 
   
   ORDER BY COUNT(*) DESC
   LIMIT 1"
